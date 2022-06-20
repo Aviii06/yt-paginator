8ef7b9a85f1776f1edab874f06beb109d0ae2c74
Aviii06 <architgosain0601@gmail.com>
2022-06-21 01:30:13 +0530

    add get request to google API

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+const { response } = require('express');
 const express = require('express');
 require('dotenv').config();
 const app = express();
+const XMLHttpRequest = require('xhr2');
+const axios = require('axios');
 
 const API_KEY = process.env.API_KEY;
+const key_word = 'sherlock';
 
-url = `https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=${API_KEY}&part=snippet,contentDetails,statistics,status`;
+url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${key_word}&type=video&key=${API_KEY}`;
 
 //Body parser
 app.use(express.json());
@@ -20,5 +24,7 @@ const router = express.Router();
 
 app.use('/',router);
 router.get('/', (req, res) => {
-    res.send(url);
-});
+
+    axios.get(url).then(r=>console.log(r)).catch(err => console.log(err));
+    res.send('yo');
+})
