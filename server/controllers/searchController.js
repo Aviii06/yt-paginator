c9322c48159b0811034151b96885db45e680d26e
Aviii06 <architgosain0601@gmail.com>
2022-06-22 15:16:39 +0530

    Optimize search query

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -3,13 +3,6 @@ const model = require('../models/searchQueries');
 exports.post = (req, res) => {
     let query = req.body.query;
     res.redirect(`search/${query}`);
-
-    // model.findAll(query).then((data)=>{
-    //     res.render('searchPage', {
-    //         data: data,
-    //         query: query
-    // })
-    // });
 };
 
 exports.getParams = (req, res) => {
--- server/models/searchQueries.js
+++ server/models/searchQueries.js
@@ -3,7 +3,10 @@ const schema = require('../database/schema.js');
 //Gets all the data with a specific search query.
 exports.findAll = async (query) => {
     return await schema.find({
-        videoTitle: { $regex: query, $options: "i" }
+        $or: [
+            { description: { $regex: query, $options: "i" } },
+            { videoTitle: { $regex: query, $options: "i" }, }
+        ]
     })
         .sort({ publishTime: -1 });
 }
